0c2dd13038560edeaf27bcd951be13dbb009cb14
shady.attia <shady.attia@cyshield.com>
2025-03-04 13:19:22 +0200

    - edit weather screen topAppbar properties

--- app/src/main/java/com/example/weatherapp/ui/component/TitleTopAppBar.kt
+++ app/src/main/java/com/example/weatherapp/ui/component/TitleTopAppBar.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -17,7 +18,9 @@ fun TitleTopAppBar(title: String, onBackButtonClick: () -> Unit) {
                 text = title,
                 fontSize = 25.sp,
                 fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center
+                textAlign = TextAlign.Center,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
             )
         },
         navigationIcon = {
